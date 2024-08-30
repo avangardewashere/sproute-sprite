e13b311d0ebd0a8e3520a93eaa0eee9ece0452a2
avan_web_okbet <avansoftify@gmail.com>
2024-08-30 18:24:13 +0800

    first commit

---
+++ app/about/page.tsx
@@ -0,0 +1,11 @@
+import Link from "next/link";
+
+export default function About() {
+    return(
+        <div>
+            <h1>About</h1>
+            <h2>Link</h2>
+            <Link href="/">Go back</Link>
+        </div>
+    )
+}
---
+++ app/products/[id]/page.tsx
@@ -0,0 +1,6 @@
+export default function Products({params}:{params:{id:string}}){
+    return(
+        <div>
+            <h1>Product {params.id}</h1>
+        </div>)
+}
